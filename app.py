cd8d559164e1785ac6a3bdb98c47df588ee8d801
marinazakimi <144032134+marinazakimi@users.noreply.github.com>
2023-10-23 14:32:56 +0000

    atualizado

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(
     'mimosa'
-    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.minijogos.com.br%2Fjogo%2Ffive-nights-at-freddys&psig=AOvVaw1UeNSktWEU-DvXY-8u2I1J&ust=1698155739600000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOib5qGpjIIDFQAAAAAdAAAAABAD'
+    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png'
 
 )
 
@@ -11,5 +11,7 @@ st.title('PAGINA DE TESTE')
 
 esportes = pd.read_csv(
     'https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv',
-     enconding= 'latin-1')
+     encoding= 'latin-1')
+
+    
 st.dataframe(esportes)
